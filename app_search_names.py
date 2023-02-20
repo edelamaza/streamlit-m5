37827a41b495c6001991f43a6554a00e3aa1b5f3
Esteban de la Maza <82958825+edelamaza@users.noreply.github.com>
2023-02-20 14:30:43 +0000

    added search names app

---
+++ app_search_names.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Search Names Example")
+
+DATA_URL = 'dataset.csv'
+
+
+@st.cache
+def load_data_by_name(name):
+    data = pd.read_csv(DATA_URL)
+    filtered_data_by_name = data[data['name'].str.contains(name)]
+    return filtered_data_by_name
+
+
+myName = st.text_input("What is your name?")
+if (myName):
+    filterbyname = load_data_by_name(myName)
+    count_row = filterbyname.shape[0]
+    st.write(f"Result : {count_row} rows")
+
+    st.dataframe(filterbyname)
