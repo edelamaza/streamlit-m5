80cf6718f7ac4b8b78c306064c7f505b94c3e2e8
Esteban de la Maza <82958825+edelamaza@users.noreply.github.com>
2023-03-10 17:38:08 +0000

    added name and student id

--- nyc.py
+++ nyc.py
@@ -2,37 +2,43 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Cicle Rides in NYC')
+st.title('Bike Rides in NYC')
+st.subheader('Esteban de la Maza Castro A01720548')
 
 DATE_COLUMN = 'started_at'
-DATA_URL = ('citibike-tripdata.csv')
+DATA_URL = 'citibike-tripdata.csv'
+
+tab1, tab2, tab3 = st.tabs(["Raw Data", "Bar Chart", "Map"])
+
 
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
     data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state = st.text('Loading cicle nyc data...')
-data = load_data(1000)
-data_load_state.text("Done! (using st.cache)")
 
-if st.sidebar.checkbox('Show raw data'):
+with st.spinner('Loading data...'):
+    data = load_data(1000)
+    st.success("Done! (using st.cache)")
+
+with tab1:
     st.subheader('Raw data')
-    st.write(data)
+    st.dataframe(data)
 
-if st.sidebar.checkbox('Recorridos por hora'):
-    st.subheader('Numero de recorridos por hora')
+with tab2:
+    st.subheader('Number of Pickups by Hour')
 
-    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    hist_values = np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
     st.bar_chart(hist_values)
 
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+with tab3:
+    # Some number in the range 0-23
+    hour_to_filter = st.slider('hour', 0, 23, 17)
+    filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+    st.map(filtered_data)
